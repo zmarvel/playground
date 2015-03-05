58cb06ca3e38e676276b0dffa921b750f53116ff
Zack Marvel <zpmarvel@gmail.com>
2015-03-05 17:29:37 -0500

    Another reddit problem I started a bit ago
    Not going to finish it now

--- meltdown/core.clj
+++ meltdown/core.clj
@@ -1,7 +1,15 @@
 (require '[clojure.string :as st])
 
+;; More interested in the hyperbinary problem than this one for the moment.
+;; I'll work on it when I get some time.
+
 (let [[x y] (st/split (read-line) #" ")]
   (for [line (repeatedly read-line) :while line]
-    
-    ))
+    (loop [w (st/split line #".")]
+      (cond
+        (= (first w) '\n') nil ; '\n' is not a char
+        (not= (first w) '.') (do
+                               (println (first w))
+                               (recur (rest w)))
+      )))
 
