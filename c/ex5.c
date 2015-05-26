2a5135fd42a5e6fa08a86aa7e972fd1b7db15155
Zack Marvel <zpmarvel@gmail.com>
2015-05-26 00:15:24 -0400

    Some c examples I did last winter

---
+++ c/ex5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/* Heyy comment. */
+
+int main(int argc, char *argv[]) {
+    int distance = 100;
+
+    // anoter comment. ;)
+    printf("You're %d miles away.\n", distance);
+
+    return 0;
+}
